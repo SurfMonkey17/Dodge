836ec7a91eccd7bff0b24993567178fb0b345c74
Sherry Fisher <sherryw17@gmail.com>
2025-02-11 17:44:45 -0600

    added variables for movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float xValue = 0f;
+    [SerializeField] float yValue = 0f;
+    [SerializeField] float zValue = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.Translate(1, 0, 0);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(xValue, yValue, zValue);
     }
 }
