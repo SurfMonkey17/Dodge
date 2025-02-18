b23152133faef89368fb0939b9da4b4c82ffa224
Sherry Fisher <sherryw17@gmail.com>
2025-02-18 14:46:10 -0600

    added PrintInstructions and MovePlayer methods

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,17 +6,26 @@ public class Mover : MonoBehaviour
 
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     void Update()
     {
+        MovePlayer();       
+    } 
+    
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Win the game by avoiding all the obstacles");
+        Debug.Log("Move using the arrow keys or WASD");
+    }
 
-        float xValue = Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime; 
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime;
         float yValue = 0;
-        float zValue = Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime; 
-       
-
+        float zValue = Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime;
         transform.Translate(xValue, yValue, zValue);
     }
 }
